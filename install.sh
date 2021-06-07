45023360c725561ae1ae006d407ac634f1e74657
bedjan <29821749+bedjan@users.noreply.github.com>
2021-06-07 17:39:58 +0200

    Add files via upload

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ which openbox || sudo apt install openbox
 which obconf || sudo apt install obconf
 which obmenu || sudo apt install obmenu
 
-sudo apt install --no-install-recommends git openbox obmenu obconf mousepad nano mc stalonetray fspanel gexec volumeicon-alsa tint2 pnmixer pulseaudio numlockx sakura stterm firefox-esr firefox-esr-l10n-cs mpv  obsidian-icon-theme gxmessage smtube smplayer smplayer-l10n mpv vlc vlc-l10n xfe xfe-i18n pcmanfm streamtuner2 calcoo dmenu mpg123 scrot screen fbreader pandoc retext
+sudo apt install --no-install-recommends git openbox obmenu obconf mousepad nano mc stalonetray fspanel gexec volumeicon-alsa tint2 pnmixer pulseaudio numlockx sakura stterm firefox-esr firefox-esr-l10n-cs mpv  obsidian-icon-theme gxmessage smtube smplayer smplayer-l10n mpv vlc vlc-l10n xfe xfe-i18n pcmanfm streamtuner2 calcoo dmenu mpg123 scrot screen fbreader pandoc retext python3-pip
+
 
 #firejail firejail-profiles firetools
--- web.md
+++ web.md
@@ -172,7 +172,7 @@ text=&act=url)
 
 [open_with](https://addons.mozilla.org/cs/firefox/addon/open-with/)
 
-mpv --fs=no  --cache-secs=60 --demuxer-readahead-secs=0 --ytdl-raw-options=no-check-certificate=
+mpv --player-operation-mode=pseudo-gui --fs=no --cache-secs=60 --demuxer-readahead-secs=0 --ytdl-raw-options=no-check-certificate=
 
 [foxy gestures](https://addons.mozilla.org/cs/firefox/addon/foxy-gestures/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search)
 
