8cb8f9488176a3ea4ff28f8f5feab71076e91b89
bedjan <29821749+bedjan@users.noreply.github.com>
2021-06-21 22:04:56 +0200

    Add files via upload

--- all.sh
+++ all.sh
@@ -151,9 +151,9 @@ case $? in
 1)
 echo "Exit";;
 2)
-xterm -T AUDIO -e  'cd /media/sda2;youtube-dl  --ignore-errors --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" "$(xsel --clipboard)"';;
+xterm -T AUDIO -e  'cd /home/d/hudba;youtube-dl  --ignore-errors --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" "$(xsel --clipboard)"';;
 3)
-xterm -T VIDEO -e'cd /media/sda2;youtube-dl --ignore-errors  "$(xsel --clipboard)"';;
+xterm -T VIDEO -e'cd /home/d/videa;youtube-dl --ignore-errors  "$(xsel --clipboard)"';;
 esac ;;
 
 
